928ac1c5dfe5fd47395eb728f066db5f70e07cc5
Jeff Hann <jeff.hann@redspace.com>
2016-05-31 13:37:14 -0300

    initial commit

---
+++ notifier.py
@@ -0,0 +1,57 @@
+import httplib2
+import os
+import subprocess
+
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'USB Notifier'
+
+def switchTo(color):
+    usblamp = subprocess.Popen(["usblamp", color],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT,
+                               close_fds=True)
+
+    return usblamp.stdout.read()
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-python-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store, flags)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    results = service.users().labels().get(userId='me', id='INBOX').execute()
+    messages = results.get('messagesUnread')
+
+    if (messages >= 0):
+        print(switchTo("red"))
+
+if __name__ == '__main__':
+    main()
